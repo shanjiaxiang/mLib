3ac28fa87520d6faef2257b6b1317ee85ed25418
shanjiaxiang <1123011216@qq.com>
2018-12-01 15:26:05 +0800

    添加sweetAlertDialog模块依赖

--- app/src/main/java/com/mit/mlib/MainActivity.java
+++ app/src/main/java/com/mit/mlib/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.mit.mlib.base.MLog;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MLog.d("hello MLog");
+
+        final SweetAlertDialog dialog = new SweetAlertDialog(this, SweetAlertDialog.NORMAL_TYPE);
+        dialog.setTitleText("dialog");
+        dialog.setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                dialog.cancel();
+            }
+        });
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
